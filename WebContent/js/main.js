20813e0ad2c6a3467f03a72c4590aeb09afa5c64
ieltxuorue <ieltxuorue@hotmail.com>
2015-06-30 12:26:41 +0200

    validar email

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -51,14 +51,6 @@ $(function() {
 
 });
 
-function validarEmail( email ) {
-    expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if ( !expr.test(email) )
-        alert("Error: La dirección de correo " + email + " es incorrecta.");
-}
-
-
-
 	/* REGISTRO USUARIOS control de usuarios existentes */
 
 function llamadaAjax(code){
